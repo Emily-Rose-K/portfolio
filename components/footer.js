77ef656c6c3d2a5f5360ae0b2d26c7acd55ec2cb
Emily Krauss <e.krauss27@gmail.com>
2020-08-07 17:20:44 -0700

    footer done

--- components/footer.js
+++ components/footer.js
@@ -5,19 +5,21 @@ export default function Footer() {
         position: "absolute",
         left:0,
         bottom:0,
-        right:0
+        right:0,
     }
     return (
         <footer style={footerstyle}>
-            <a href="https://www.linkedin.com/in/emily-krauss-89193510b/">
-                <img alt="LinkedIn" src="/github-logo.png"
-                width="40" height="40"></img>
-            </a>
-            <a href="https://github.com/Emily-Rose-K">
-                <img alt="LinkedIn" src="/linkedin-logo.png"
-                width="40" height="40"></img>
-            </a>
-            Emily Rose Krauss ©2020
+            <div id="footer-icons">
+                <a href="https://www.linkedin.com/in/emily-krauss-89193510b/" target="_blank">
+                    <img alt="LinkedIn" src="/github-logo.png"
+                    width="40" height="40"></img>
+                </a>
+                <a href="https://github.com/Emily-Rose-K" target="_blank">
+                    <img alt="LinkedIn" src="/linkedin-logo.png"
+                    width="40" height="40"></img>
+                </a>
+            </div>
+           <p id="signature">Emily Rose Krauss ©2020</p>
         </footer>
     )
 }
